e25800bc491e8f592c71f50198862717db08f4c8
Natalya Ryzheva <tyta-ryzheva@yandex.ru>
2022-11-06 18:28:51 +0300

    Хранение данных

--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -1,6 +1,8 @@
 package ru.netology.nmedia
 
+import android.app.Application
 import androidx.core.view.isVisible
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -18,8 +20,8 @@ private val empty = Post(
 
 )
 
-class PostViewModel : ViewModel() {
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryFilesImpl(application)
     val data: LiveData<List<Post>> = repository.get()
     val edited = MutableLiveData(empty)
 
